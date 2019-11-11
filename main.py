6e8fdbeddfdf58f0ed043560e11dce95c55b06e7
Leonardo Maccari <leonardo.maccari@unitn.it>
2019-11-11 11:45:21 +0100

    several different imports

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ from mypackage import mysecondmodule
 
 myfirstmodule.myfirstfunction()
 mysecondmodule.mysecondfunction()
+
+# Note that imports should be all done at the beginning of the file
+# I place them in the middle to help you understand the different 
+# import statements
+
+from mypackage.myfirstmodule import myfirstfunction
+myfirstfunction()
+
+from mypackage.myfirstmodule import myfirstfunction as myf
+myf()
+
+#BAD PRACTICE!
+from mypackage import *
+myfirstmodule.myfirstfunction()
